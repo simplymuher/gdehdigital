13df3c73c096633f506441d73ebdbaf61eacd67f
simplymuher <simplymuher@gmail.com>
2026-06-21 01:07:44 +0300

    Add DNS module and set default result order

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
 const express = require("express");
 const session = require("express-session");
 const { Pool } = require("pg");
